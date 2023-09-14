eb8787447708c46e8d02d6b0dd1debb62e53f896
Adamyyyy <happyadam2008@gmail.com>
2023-09-14 17:36:34 +1000

    first commit

---
+++ components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const HeaderWraper = styled.header`
+  background-color: #333;
+  color: #fff;
+  padding: 1rem;
+`;
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Header</h1>
+    </header>
+  );
+};
+
+export default Header;
